4e1a8bebc509f208c35446648c420eb59642614e
xeolabs <lindsay.kay@xeolabs.com>
2022-09-08 01:49:23 +0200

    Updates

--- convert.js
+++ convert.js
@@ -111,21 +111,21 @@ const configs = JSON.parse(configsData);
             fs.appendFileSync(logEnterprise1PathAbs, `\n\n# convert2xkt\n\n${configs.paths["convert2xkt"]} -s ${glbEnterprise1PathAbs} -m ${jsonEnterprise1PathAbs} -o ${xktEnterprise1PathAbs} -l \n`);
             execSync(`node ${configs.paths["convert2xkt"]} -s ${glbEnterprise1PathAbs} -m ${jsonEnterprise1PathAbs} -o ${xktEnterprise1PathAbs} -l >> ${logEnterprise1PathAbs}`, {stdio: 'inherit'});
 
-            const communityPipeline1Docs = "https://www.notion.so/xeokit/Converting-IFC-Models-to-XKT-using-3rd-Party-Open-Source-Tools-c373e48bc4094ff5b6e5c5700ff580ee";
-            const communityPipeline2Docs = "https://www.notion.so/xeokit/Converting-Models-to-XKT-with-convert2xkt-fa567843313f4db8a7d6535e76da9380";
-            const enterprisePipeline1Docs = "https://www.notion.so/xeokit/Converting-IFC-to-XKT-using-ifc2gltf-a2e0005d00dc4f22b648f1237bc3245d";
+            const communityPipeline1Docs = "pipelines#community-pipeline-1";
+            const communityPipeline2Docs = "pipelines#community-pipeline-2";
+            const enterprisePipeline1Docs = "pipelines#enterprise-pipeline-1";
 
-            m.push(`### ${fileName}\n
+            m.push(`\n### ${fileName}\n\n
 | View Model | Conversion Pipeline | Conversion Log |
 | --- | --- | --- |
 | [.ifc](viewModel.html?src=converted/${fileName}/model.ifc) | [WebIFCLoaderPlugin](https://xeokit.github.io/xeokit-sdk/docs/class/src/plugins/WebIFCLoaderPlugin/WebIFCLoaderPlugin.js~WebIFCLoaderPlugin.html) | |
-| [.xkt](viewModel.html?src=converted/${fileName}/community1/model.xkt) | [Community Pipeline 1](${communityPipeline1Docs}) | [Log](converted/${fileName}/community1/log.txt) |
-| [.xkt](viewModel.html?src=converted/${fileName}/community2/model.xkt) | [Community Pipeline 2](${communityPipeline2Docs}) | [Log](converted/${fileName}/community2/log.txt) |
-| [.xkt](viewModel.html?src=converted/${fileName}/enterprise1/model.xkt)| [Enterprise Pipeline 1](${enterprisePipeline1Docs}) | [Log](converted/${fileName}/enterprise1/log.txt) |
-| [.glb](viewModel.html?src=converted/${fileName}/community1/model.glb) | [Community Pipeline 1](${communityPipeline1Docs}) | [Log](converted/${fileName}/community1/log.txt) |
-| [.glb](viewModel.html?src=converted/${fileName}/enterprise1/model.glb) | [Enterprise Pipeline 1](${enterprisePipeline1Docs}) | [Log](converted/${fileName}/enterprise1/log.txt) |
-| [.glb + .json](viewModel.html?src=converted/${fileName}/community1/model.glb&metaModelSrc=converted/${fileName}/community1/model.json) | [Community Pipeline 1](${communityPipeline1Docs})  | [Log](converted/${fileName}/community1/log.txt) |
-| [.glb + .json](viewModel.html?src=converted/${fileName}/enterprise1/model.glb&metaModelSrc=converted/${fileName}/enterprise1/model.json) | [Enterprise Pipeline 1](${enterprisePipeline1Docs})  | [Log](converted/${fileName}/enterprise1/log.txt) |`);
+| [.xkt](viewModel.html?src=converted/${fileName}/community1/model.xkt) | [Community Pipeline #1](${communityPipeline1Docs}) | [Log](converted/${fileName}/community1/log.txt) |
+| [.xkt](viewModel.html?src=converted/${fileName}/community2/model.xkt) | [Community Pipeline #2](${communityPipeline2Docs}) | [Log](converted/${fileName}/community2/log.txt) |
+| [.xkt](viewModel.html?src=converted/${fileName}/enterprise1/model.xkt)| [Enterprise Pipeline #1](${enterprisePipeline1Docs}) | [Log](converted/${fileName}/enterprise1/log.txt) |
+| [.glb](viewModel.html?src=converted/${fileName}/community1/model.glb) | [Community Pipeline #1](${communityPipeline1Docs}) | [Log](converted/${fileName}/community1/log.txt) |
+| [.glb](viewModel.html?src=converted/${fileName}/enterprise1/model.glb) | [Enterprise Pipeline #1](${enterprisePipeline1Docs}) | [Log](converted/${fileName}/enterprise1/log.txt) |
+| [.glb + .json](viewModel.html?src=converted/${fileName}/community1/model.glb&metaModelSrc=converted/${fileName}/community1/model.json) | [Community Pipeline #1](${communityPipeline1Docs})  | [Log](converted/${fileName}/community1/log.txt) |
+| [.glb + .json](viewModel.html?src=converted/${fileName}/enterprise1/model.glb&metaModelSrc=converted/${fileName}/enterprise1/model.json) | [Enterprise Pipeline #1](${enterprisePipeline1Docs})  | [Log](converted/${fileName}/enterprise1/log.txt) |`);
         }
 
         fs.writeFileSync("./index.md", m.join("\n"), {encoding: 'utf8'});
--- pipelines.md
+++ pipelines.md
@@ -5,7 +5,7 @@
 
 ---
 
-This converter pipeline uses all open source tools to convert IFC files to XKT.
+This ````.ifc```` to ````.xkt```` conversion pipeline uses all open source tools, and creates intermediate ````.glb```` and ````.json```` files.
 
 #### Tools used
 
@@ -24,7 +24,7 @@ This converter pipeline uses all open source tools to convert IFC files to XKT.
 
 ## Community Pipeline #2
 
-This pipeline also uses a single open source tool to directly convert IFC files to XKT.
+This pipeline also uses a single open source tool to directly convert ````.ifc```` files to ````.xkt````.
 
 #### Tools used
 
@@ -40,11 +40,13 @@ This pipeline also uses a single open source tool to directly convert IFC files 
 
 ## Enterprise Pipeline #1
 
-This pipeline uses a combination of open source and proprietary tools to convert IFC to XKT. This pipeline has the best performance for large IFC models.
+This pipeline uses a combination of proprietary and open source tools to convert ````.ifc```` to ````.xkt````.  Like *Community Pipeline #1*, this pipeline also creates intermediate ````.glb```` and ````.json```` files. 
+
+This pipeline has the best performance for large IFC models.
 
 #### Tools used
 
-* [ifc2gltf]() (Closed source)
+* [ifc2gltf](https://creoox.com/en/contact/) (Closed source)
 * [convert2xkt](https://github.com/xeokit/xeokit-convert) (AGPL3)
 
 #### More info
